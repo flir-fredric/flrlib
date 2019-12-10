f887df4fe855d0a09f19daf436df7ce961a40201
robert <robert.karlsson@flir.com>
2019-12-10 12:29:32 +0100

    Changed dev entry.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/prod.js')
 } else {
-  module.exports = require('./dist/dev.js')
+  module.exports = require('./components')
 }
